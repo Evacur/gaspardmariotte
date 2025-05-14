3f9f9a4af2d2cc2ce74d85760ee73024efb4a53b
Evacur <cuervapbl@gmail.com>
2025-05-14 19:16:20 +0200

    Comint

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="absolute top-0 w-full z-10 text-white px-6 py-4 flex justify-between items-center">
+    <header className="absolute top-0 w-full z-10 text-black px-6 py-4 flex justify-between items-center">
      <img src="/Cinco.png" alt="Logo Gaspard Mariotte" className="h-10" />
       <nav className="flex gap-6 text-sm font-medium">
         <a href="#creations" className="hover:underline">Créations</a>
