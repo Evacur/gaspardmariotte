beaf62eb2f63e25fb17ab9644c109f2026be30e0
Evacur <cuervapbl@gmail.com>
2025-05-14 18:16:49 +0200

    T

---
+++ components/header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="absolute top-0 w-full z-10 text-white px-6 py-4 flex justify-between items-center">
+     <img src="/Cinco.png" alt="Logo Gaspard Mariotte" className="h-10" />
+      <nav className="flex gap-6 text-sm font-medium">
+        <a href="#creations" className="hover:underline">Créations</a>
+        <a href="#collaborations" className="hover:underline">Collaborations</a>
+        <a href="#expositions" className="hover:underline">Expositions</a>
+      </nav>
+    </header>
+  )
+}
+
