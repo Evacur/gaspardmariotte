9260c1a64f63bde8b4847960c91862ecc40c539e
Evacur <cuervapbl@gmail.com>
2025-06-06 10:20:19 +0200

    Màj_5

--- lib/sanity.tsx
+++ lib/sanity.tsx
@@ -6,6 +6,7 @@ export const config = {
   dataset: 'production',
   apiVersion: '2023-01-01',
   useCdn: true,
+  token: process.env.skcuwaCMSPPqESLP8ONOuFqHM254K9NrEGH9QisA7eVc4dSPitCexzeDgvWfmT3Y7ORfZztuXEF1pDv1wM3ktvK0U5VB4lwwCDVAgWZ5IQxrYS5L2Cb5afz3umOAyvSmWZ7F3ZL6symshezFQRxZnymbCwgtBU1UtANlXTeBk9R8ozviZMDw,
 }
 
 export const client = createClient(config)
