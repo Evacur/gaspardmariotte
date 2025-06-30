0c99d25c9baf729daf92196bcb8ecb081e4b6538
Evacur <cuervapbl@gmail.com>
2025-06-30 15:45:05 +0200

    Token

--- lib/sanity.tsx
+++ lib/sanity.tsx
@@ -2,13 +2,14 @@ import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const config = {
-  projectId: '730lhcwc',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '730lhcwc',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2023-01-01',
-  useCdn: true,
-  token: process.env.skcuwaCMSPPqESLP8ONOuFqHM254K9NrEGH9QisA7eVc4dSPitCexzeDgvWfmT3Y7ORfZztuXEF1pDv1wM3ktvK0U5VB4lwwCDVAgWZ5IQxrYS5L2Cb5afz3umOAyvSmWZ7F3ZL6symshezFQRxZnymbCwgtBU1UtANlXTeBk9R8ozviZMDw,
+  useCdn: process.env.NODE_ENV === 'production',
+  token: process.env.SANITY_API_TOKEN,
 }
 
 export const client = createClient(config)
 
-export const urlFor = (source: any) => imageUrlBuilder(config).image(source)
+export const urlFor = (source: any) =>
+  imageUrlBuilder(config).image(source)
